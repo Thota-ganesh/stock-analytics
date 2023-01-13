070e3ee945162f49e89b19ffea5dd9ff83b5cf7d
Satish <satish@example.com>
2023-01-12 20:24:46 -0500

    Set version. Rename root package. Add default config

--- src/main/java/io/endeavour/stocks/StockAnalyticsApplication.java
+++ src/main/java/io/endeavour/stocks/StockAnalyticsApplication.java
@@ -1,4 +1,4 @@
-package io.endeavour;
+package io.endeavour.stocks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/io/endeavour/stocks/StockAnalyticsApplicationTests.java
+++ src/test/java/io/endeavour/stocks/StockAnalyticsApplicationTests.java
@@ -1,4 +1,4 @@
-package io.endeavour;
+package io.endeavour.stocks;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
