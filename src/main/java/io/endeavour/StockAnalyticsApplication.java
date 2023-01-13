2f920a7131cfa0a6d1bef5abdb0bcac72ab14667
Satish <satish@example.com>
2023-01-12 20:18:15 -0500

    Initialize project

---
+++ src/main/java/io/endeavour/StockAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package io.endeavour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalyticsApplication.class, args);
+	}
+
+}
